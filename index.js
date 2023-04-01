f88d7588d1140d7a2a5e5aedad644b0160fd1511
erickariuki <eric.kariuki@student.moringaschool.com>
2023-04-01 19:22:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+var namesArray = ["Guadalupe", "Ollie", "Aki"];
+var event = "birthday"
+var cards = [];
+
+function writeCards(namesArray, event){
+   
+    for(var i = 0; i < namesArray.length; i++){
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+
+    }
+
+    return cards;
+}
+
+function countDown(num) {
+    while (num >= 0){
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(num)
